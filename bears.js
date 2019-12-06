e121b8c27f6b1c34fadda4d88f651bb81d873f57
Zachary <zachjoliver@gmail.com>
2019-12-05 23:24:59 -0800

    Working version

--- bears.js
+++ bears.js
@@ -1,10 +1,14 @@
 function bears(x, s) {
   let pairs = "";
 
-  for (let i = 0; i < s.length; i++) {
-
+  for (let i = 0; i < s.length - 1; i++) {
+    let pair = s[i] + s[i + 1];
+    if (pair === "B8" || pair === "8B") {
+      pairs = pairs + pair;
+      i++;
+    }
   }
 
   const match = pairs.length / 2 >= x;
-  return [pairs,];
+  return [pairs, match];
 }
